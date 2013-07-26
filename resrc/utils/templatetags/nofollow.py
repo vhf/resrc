b9d2ce03578043191b31a5acb372ad28e27a4734
vhf <victorfelder@gmail.com>
2013-07-26 20:09:46 +0200

    Fixed various redirects {{ next }}

--- resrc/utils/templatetags/nofollow.py
+++ resrc/utils/templatetags/nofollow.py
@@ -10,7 +10,10 @@ S_NOFOLLOW = r'<\1 rel="nofollow"\2'
 
 
 class NofollowPostprocessor(Postprocessor):
-    """ TODO: Would be really nice to add nofollow only to external links"""
+    """
+    TODO: Would be really nice to add nofollow only to external links
+    If we do that, we could then append "external" to "nofollow" for external links.
+    """
     def run(self, text):
         return R_NOFOLLOW.sub(S_NOFOLLOW, text)
 
