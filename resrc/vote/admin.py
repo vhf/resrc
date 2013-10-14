d06a15bfda2c5035fec40220f9215eed4453bce7
Victor Felder <victorfelder@gmail.com>
2013-10-14 11:14:07 +0200

    Vote admin panel

--- resrc/vote/admin.py
+++ resrc/vote/admin.py
@@ -3,4 +3,19 @@ from django.contrib import admin
 
 from resrc.vote.models import Vote
 
-admin.site.register(Vote)
+def vote_title(vote):
+    if vote.link is not None and vote.alist is not None:
+        return ("%s upvoted Link AND List : %s" % (vote.user, vote.link, vote.alist))
+    elif vote.link is not None:
+        return ("%s upvoted Link : %s" % (vote.user, vote.link))
+    elif vote.alist is not None:
+        return ("%s upvoted List : %s" % (vote.user, vote.alist))
+
+
+class VoteAdmin(admin.ModelAdmin):
+    list_display = (vote_title,)
+
+    def get_form(self, request, obj=None):
+        return super(VoteAdmin, self).get_form(request, obj)
+
+admin.site.register(Vote, VoteAdmin)
