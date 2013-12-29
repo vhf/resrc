b7babbb8286da527d47abd38b61f92fdb1f58a34
Victor Felder <victorfelder@gmail.com>
2013-12-29 16:04:49 +0100

    Improve list admin

--- resrc/list/admin.py
+++ resrc/list/admin.py
@@ -4,18 +4,13 @@ from django.contrib import admin
 from resrc.list.models import List, ListLinks
 
 
-def list_name(alist):
-    return ("%s, by %s" % (alist.title, alist.owner))
-list_name.short_description = 'Name'
-
-
 class ListLinksInline_adddate(admin.TabularInline):
     model = ListLinks
     extra = 1
 
 
 class ListAdmin(admin.ModelAdmin):
-    list_display = (list_name,)
+    list_display = ('title', 'owner', 'is_public')
 
     def get_form(self, request, obj=None):
         self.inlines = (ListLinksInline_adddate,)
