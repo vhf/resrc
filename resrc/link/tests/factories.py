56ba97b80349df1ee17c0404d5d7d6db8c9380b4
Victor Felder <victorfelder@gmail.com>
2014-05-17 14:44:54 +0200

    Some poorly designed tests

--- resrc/link/tests/factories.py
+++ resrc/link/tests/factories.py
@@ -2,6 +2,8 @@ import factory
 
 from resrc.link.models import Link
 from resrc.userprofile.tests.factories import UserFactory
+from resrc.language.tests.factories import LanguageFactory
+from taggit.models import Tag
 
 
 class LinkFactory(factory.Factory):
@@ -10,3 +12,16 @@ class LinkFactory(factory.Factory):
     title = "Single page, Simple, Comprehensive Overview of Javascript"
     url = "http://betterexplained.com/articles/the-single-page-javascript-overview/"
     author = factory.SubFactory(UserFactory)
+    language = factory.SubFactory(LanguageFactory)
+    level = 'beginner'
+
+    @factory.post_generation
+    def tags(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of tags were passed in, use them
+            for tag in extracted:
+                self.tags.add(tag)
--- resrc/link/tests/models_tests.py
+++ resrc/link/tests/models_tests.py
@@ -12,6 +12,8 @@ class UserprofileTestCase(TestCase):
         self.profile = ProfileFactory()
         self.user = self.profile.user
 
+        self.profile2 = ProfileFactory()
+        self.user2 = self.profile2.user
 
     def test_slug(self):
         """test link slug"""
@@ -27,3 +29,31 @@ class UserprofileTestCase(TestCase):
         link.save()
         self.assertEqual(
             link.slug, 'single-page-simple-comprehensive-overview-of-javascript-1')
+
+    def test_lang(self):
+        link = LinkFactory()
+        link.author_id = self.user.pk
+        self.assertEqual(link.get_lang(), 'English')
+
+    def test_get_votes(self):
+        user = UserFactory()
+        user.save()
+        profile = ProfileFactory()
+        profile.user = user
+        profile.save()
+        link = LinkFactory()
+        link.author_id = user.pk
+        self.assertEqual(link.get_votes(), 0)
+
+        link.vote(user)
+        self.assertEqual(link.get_votes(), 1)
+
+        link.unvote(user)
+        self.assertEqual(link.get_votes(), 0)
+
+    def test_categories(self):
+        link = LinkFactory.create(True, ("article"))
+        link.author_id = self.user.pk
+        link.save()
+        self.assertEqual(link.get_categories(), "")
+
