6ea45c33ea4911ebd5492fdfb6aaa5e76d5873e8
Victor Felder <victorfelder@gmail.com>
2014-05-18 13:01:09 +0200

    Fix watchmedo.sh

--- watchmedo.sh
+++ watchmedo.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-gnome-shell
-#watchmedo shell-command --recursive --ignore-directories --patterns="*.py" --wait --command='fab test'
+watchmedo shell-command --recursive --ignore-directories --patterns="*.py" --wait --command='fab test'
