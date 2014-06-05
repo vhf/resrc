2c63dd73d4cc2d454289907ee6a866837b177355
Victor Felder <victorfelder@gmail.com>
2014-06-05 15:29:54 +0200

    Avoid timeouts on the server

--- resrc/settings.py
+++ resrc/settings.py
@@ -231,6 +231,8 @@ HAYSTACK_CONNECTIONS = {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
         'URL': 'http://127.0.0.1:9200/',
         'INDEX_NAME': 'haystack',
+        'TIMEOUT': 120,
+        'BATCH_SIZE': 100,
     },
 }
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
